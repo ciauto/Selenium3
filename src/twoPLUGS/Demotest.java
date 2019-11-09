eca4ce922c3f091db2cac45c6724954ac823c8c8
ciauto <naresh.busyqa@gmail.com>
2019-11-09 14:45:37 -0500

    naresh added

---
+++ src/twoPLUGS/Demotest.java
@@ -0,0 +1,59 @@
+package twoPLUGS;
+
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Demotest {
+	private WebDriver driver;
+	private String baseurl;
+	
+  @BeforeMethod
+  public void setup() {
+	  System.setProperty("webdriver.gecko.driver", "C:\\driver\\geckodriver-v0.24.0\\geckodriver.exe");
+	  baseurl="https://www.twoplugs.com/";
+	  driver = new FirefoxDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+	  driver.get(baseurl);
+    }
+  
+  @Test
+  public void verifyinvalidLogin() {
+	  driver.findElement(By.xpath("html/body/div[1]/header/div/ul/li[1]/a")).click();
+	  driver.findElement(By.xpath("//input[@id='signInEmail']")).sendKeys("Naresh_rp");
+	  driver.findElement(By.xpath("//input[@id='signInPassword']")).sendKeys("Naresh123");
+	  driver.findElement(By.xpath("//button[contains(.,'LOG IN')]")).click();
+	  
+	  WebElement text = null;
+		  try 
+		  	{
+			text = driver.findElement(By.xpath("//span[text()='Hi,naresh_rp']"));
+		  	}
+	  	  catch(NoSuchElementException e)
+		  	{
+	  		  System.out.println(e.getMessage());
+		  	}
+	 Assert.assertTrue(text != null);
+  }
+  
+  @AfterMethod
+  public void teardown(ITestResult testResult) throws IOException {
+	  if(testResult.getStatus() == ITestResult.FAILURE) 
+	  {
+		  twoPLUGS.Screenshot.takeScreenshot(driver, testResult.getName());
+	  }
+	  driver.quit();
+ }
+}
+  
